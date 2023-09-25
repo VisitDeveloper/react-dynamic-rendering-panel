392987b3eea95a881c9ef0108ecfd52557686f11
a.hatami <a.hatami@vasl.ir>
2023-09-25 12:43:17 +0330

    add package

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 import Main from "page"
 import { useState } from "react"
 import  {Switch}  from "switch-custom-design"
-// import * as Switch  from "switch-custom-design"
+
 function App() {
   const [toggle ,setToggle] = useState<boolean>(false)
   return (
